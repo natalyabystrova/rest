06ec5ca134d72c8995f7951e6c63e68531bfd18e
[tash] <info@geekbrains.ru>
2023-01-22 23:24:04 +0300

    lesson_6

--- librari/urls.py
+++ librari/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework.authtoken import views
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from app.views import AuthorModelViewSet
-from app.views import AuthorModelViewSet, ProjectModelViewSet, ToDoModelViewSet
 from django.conf.urls import include
 from app.views import ProjectModelViewSet, ToDoModelViewSet, ProjectListAPIView
 
@@ -31,4 +30,5 @@ router.register('projectlist', ProjectListAPIView)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',  include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
