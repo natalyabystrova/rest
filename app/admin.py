dae4c57c187d2a6268f74a5552aee70a3d5fa185
[tash] <info@geekbrains.ru>
2023-02-13 14:22:27 +0300

    less_7

--- app/admin.py
+++
@@ -1,7 +0,0 @@
-from app import models
-from django.contrib import admin
-
-admin.site.register(models.Author)
-admin.site.register(models.Project)
-admin.site.register(models.ToDo)
-admin.site.register(models.Book)
